77403356dc42010d3e14959b686122d878d50111
AylinMtz03 <144825880+AylinMtz03@users.noreply.github.com>
2025-10-28 09:23:47 -0600

    aylin

--- algoweb/wallet/models.py
+++ algoweb/wallet/models.py
@@ -6,8 +6,16 @@ from django.contrib.auth.models import User
 
 class Wallet(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    address = models.CharField(max_length=58)  
-    private_key = models.TextField() 
-
+    address = models.CharField(max_length=58)
+    private_key = models.TextField()
     def __str__(self):
         return f"Wallet de {self.user.username}"
+    
+
+class Contacto(models.Model):
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField()
+    wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE)
+    contact_wallet = models.CharField(max_length=58, blank=True, null=True)
+    def __str__(self):
+        return f"Contacto: {self.nombre} ({self.wallet.user.username})"
